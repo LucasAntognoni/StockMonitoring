aa7db9063ad5371553647dd76b0838fccd9ae08f
Lucas Antognoni <antognoni.lucas@gmail.com>
2023-07-27 00:58:40 -0300

    Reading target ticker from command line args

--- StockMonitor/PriceTracker/Worker.cs
+++ StockMonitor/PriceTracker/Worker.cs
@@ -3,13 +3,18 @@ namespace PriceTracker.Services;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+
     private readonly ApiService _apiService;
+
+    private readonly string _ticker;
 
     public Worker(ILogger<Worker> logger, ApiService apiService)
     {
         _logger = logger;
 
         _apiService = apiService;
+
+        _ticker = Environment.GetCommandLineArgs()[1];
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -18,7 +23,7 @@ public class Worker : BackgroundService
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            await _apiService.GetStockData("");
+            await _apiService.GetStockData(_ticker);
 
             await Task.Delay(900000, stoppingToken);
         }
