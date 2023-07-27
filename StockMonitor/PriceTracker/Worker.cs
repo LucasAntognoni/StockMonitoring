a319fd9a0fd335827eb2d1eaf7fe40db18a4d75e
Lucas Antognoni <antognoni.lucas@gmail.com>
2023-07-27 00:56:59 -0300

    Added 'ApiService' to worker

--- StockMonitor/PriceTracker/Worker.cs
+++ StockMonitor/PriceTracker/Worker.cs
@@ -1,20 +1,26 @@
-namespace PriceTracker;
+namespace PriceTracker.Services;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly ApiService _apiService;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, ApiService apiService)
     {
         _logger = logger;
+
+        _apiService = apiService;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Starting service...");
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            await _apiService.GetStockData("");
+
+            await Task.Delay(900000, stoppingToken);
         }
     }
 }
