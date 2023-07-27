73471ef5a471863f141e5a356d37ce72c07e764f
Lucas Antognoni <antognoni.lucas@gmail.com>
2023-07-27 00:43:59 -0300

    Loading settings, command line args and worker service

--- StockMonitor/PriceTracker/Program.cs
+++ StockMonitor/PriceTracker/Program.cs
@@ -1,7 +1,13 @@
 using PriceTracker;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureHostConfiguration(hostConfig =>
+    {
+        hostConfig.SetBasePath(Directory.GetCurrentDirectory());
+        hostConfig.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        hostConfig.AddCommandLine(args);
+    })
+    .ConfigureServices((context, services) =>
     {
         services.AddHostedService<Worker>();
     })
