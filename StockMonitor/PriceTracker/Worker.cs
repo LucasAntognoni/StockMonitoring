85bfd5a909089c76707c2f2b066220b8b2499177
Lucas Antognoni <antognoni.lucas@gmail.com>
2023-07-30 18:20:26 -0300

    Arguments validation, ticker validation, mailing and error handling

--- StockMonitor/PriceTracker/Worker.cs
+++ StockMonitor/PriceTracker/Worker.cs
@@ -1,3 +1,5 @@
+using PriceTracker.Models;
+
 namespace PriceTracker.Services;
 
 public class Worker : BackgroundService
@@ -19,9 +21,66 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("Starting service...");
+        try
+        {
+            #region Arguments validation
+            string [] arguments = Environment.GetCommandLineArgs();
 
-        while (!stoppingToken.IsCancellationRequested)
+            if(arguments.Length != 4){
+                throw new Exception("Missing arguments!");
+            }
+            
+            string  ticker       = arguments[1];
+            decimal sellingPrice = Decimal.Parse(arguments[2]);
+            decimal buyingPrice  = Decimal.Parse(arguments[3]);
+            #endregion
+
+            // Validate ticker
+            bool valid = await _apiService.ValidateTicker(ticker);
+            if(!valid)
+            {
+                throw new Exception("Invalid ticker!");
+            }
+
+            decimal currentPrice = 0.0m;
+            int     currentDate  = 0;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Get price
+                HistoricalDataPrice? data = await _apiService.GetTickerPrice(ticker, currentDate);
+                
+                if(data != null)
+                {  
+                    // In some cases only the 'Date' field has a value
+                    if (data.Close != null)
+                    {
+                        currentPrice = (decimal)data.Close;
+                    }
+                    
+                    currentDate = data.Date;
+
+                    // Check price
+                    if (currentPrice > sellingPrice)
+                    {
+                        await _emailService.SendEmail("SELL", ticker, sellingPrice, currentPrice);
+                    }
+
+                    if (currentPrice < buyingPrice)
+                    {
+                        await _emailService.SendEmail("BUY", ticker, sellingPrice, currentPrice);    
+                    }
+                }
+
+                // Run every minute
+                await Task.Delay(60000, stoppingToken);
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Exception while running application: {exception}", e);
+        }
+        finally 
         {
             _hostApplicationLifetime.StopApplication();
         }
